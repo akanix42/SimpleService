21765dccef632f69471161a123f4d6c14ac4ab44
Nathan Reid <nate@nateandmor.com>
2014-02-14 15:58:37 -0500

    Initial commit

---
+++ src/SimpleService.Core/CustomServiceBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceProcess;
+
+namespace SimpleService
+{
+    public class CustomServiceBase : ServiceBase
+    {
+        public void StartService(string[] args)
+        {
+            OnStart(args);
+        }
+
+        public void StopService()
+        {
+            OnStop();
+        }
+    }
+}
---
+++ src/SimpleService.Core/ServiceManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.ServiceProcess;
+
+namespace SimpleService
+{
+    public delegate void ServiceManagerHandler(object sender, EventArgs args);
+
+    public class ServiceManager<T> where T : CustomServiceBase, new()
+    {
+        public event ServiceManagerHandler ServiceDefaultModeStarting;
+        public event ServiceManagerHandler ServiceInteractiveModeStarting;
+
+        public void Run(bool showConsole = false)
+        {
+            if (!showConsole && !Debugger.IsAttached)
+            {
+                StartServiceDefault();
+            }
+            else
+            {
+                StartServiceInteractive();
+            }
+        }
+
+        private void StartServiceDefault()
+        {
+            if (ServiceDefaultModeStarting != null)
+                ServiceDefaultModeStarting(this, null);
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+                { 
+                    new T() 
+                };
+            ServiceBase.Run(ServicesToRun);
+        }
+
+        private void StartServiceInteractive()
+        {
+            if (ServiceInteractiveModeStarting != null)
+                ServiceInteractiveModeStarting(this, null);
+            Console.Title = Assembly.GetEntryAssembly().FullName;
+            Console.WriteLine(Assembly.GetEntryAssembly().FullName);
+            T service = new T();
+
+            Console.WriteLine();
+            try
+            {
+                Console.WriteLine();
+                Console.WriteLine("Calling StartService()");
+                Console.WriteLine("-----------------------------------------------");
+
+                service.StartService(null);
+
+                Console.WriteLine();
+                Console.WriteLine("Service running in foreground, press enter to exit...");
+                Console.ReadLine();
+
+                Console.WriteLine("Calling StopService()");
+                Console.WriteLine("-----------------------------------------------");
+
+                service.StopService();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
